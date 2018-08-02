1e19d6fbdbf185a8955533780324940e7a9bed22
Jamie Nicol <jamie@jamienicol.me>
2018-08-02 08:41:59 +0100

    emacs: auto save to current file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -9,7 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auto-save-visited-file-name nil)
+ '(auto-save-visited-mode t)
  '(column-number-mode t)
  '(company-backends
    (quote
