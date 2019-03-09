d927c2ebb9cb1006668db7b6b9194f26d0d9314f
Jamie Nicol <jamie@jamienicol.me>
2019-03-09 13:48:27 +0000

    emacs: colorize output in compilation mode
    Adds colour, but more importantly stops escape characters from getting
    in the way.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -133,3 +133,13 @@
 (add-hook 'rust-mode-hook #'lsp)
 (if (not (eq system-type 'windows-nt))
     (add-hook 'c++-mode-hook #'lsp))
+
+(require 'ansi-color)
+(defun endless/colorize-compilation ()
+  "Colorize from `compilation-filter-start' to `point'."
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region
+     compilation-filter-start (point))))
+
+(add-hook 'compilation-filter-hook
+          #'endless/colorize-compilation)
